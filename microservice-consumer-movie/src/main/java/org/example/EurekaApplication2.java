faca9d650a2f4f27c51d01f9e81a560cb474c3bf
cuiwan <328249403@qq.com>
2020-12-30 22:51:14 +0800

    测试 git 提交

--- microservice-consumer-movie/src/main/java/org/example/EurekaApplication2.java
+++ microservice-consumer-movie/src/main/java/org/example/EurekaApplication2.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class EurekaApplication2 {
+    // git 测试
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication2.class);
     }
